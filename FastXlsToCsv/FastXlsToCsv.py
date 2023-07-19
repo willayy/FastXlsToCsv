1d7f881af51604f3b645646afb458c57b8dcc835
William Norland <william.norland@seb.se>
2023-07-19 17:10:03 +0200

    adding method stubs for FastXlsToCsv

---
+++ FastXlsToCsv/FastXlsToCsv.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def convertXlsDirToCsv(inputDir: str, outputDir: str) -> None:
+    "Convert a dir of .xls to csv"
+
+def convertXlsToCsv(inputDir: str, outputDir: str) -> None:
+    "Convert a single .xls file to csv"
+
+def convertXlsxDirToCsv(inputDir: str, outputDir: str) -> None:
+    "Convert a dir of .xlsx to csv"
+
+def convertXlsxToCsv(inputDir: str, outputDir: str) -> None: 
+    "Convert a single .xslx to csv"
