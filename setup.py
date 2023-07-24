ead6df7722267955ab95ab1fea5857ca4e301f8e
William Norland <william.norland@seb.se>
2023-07-24 13:29:46 +0200

    Finalizing

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='FastXlsToCsv',
     version='1.0.0',
-    description='A fast way to convert .xls and .xlsx to csv with vba',
+    description='A fast way to convert .xls and .xlsx to csv with vbs',
+    long_description= long_description,
+    url= 'https://github.com/willayy/FastXlsToCsv',
+    license= 'MIT',
+    author_email= 'williamnorland@gmail.com',
     author='William Norland',
     packages=find_packages(),
-    install_requires=[],  # Add any dependencies here
+    install_requires=[],  # Add any dependencies here,
+    classifiers= ['License :: OSI Approved :: MIT License',
+                  'Programming Language :: Python',
+                  'Programming Language :: Python :: 3',
+                  'Programming Language :: Python :: 3.11']
 )
--- test.py
+++
@@ -1,7 +0,0 @@
-from FastXlsToCsv import XlsConverter
-
-
-input = r"c:\Users\S3136G\Desktop\New folder\aaa.xlsx"
-output =  r"c:\Users\S3136G\Desktop\ImBassssinh"
-XlsConverter.convertXlFileToCsv(input, output)
-
