d1f9285120a50adbf7580d3a5b360bacff5b3630
William Norland <william.norland@seb.se>
2023-07-24 14:51:05 +0200

    trying to untrack

---
+++ build/lib/FastXlsToCsv/_XlsToCsvExceptions.py
@@ -0,0 +1,23 @@
+"""
+Just custom FastXlsToCsv exceptions to make debugging when using this module easier.
+"""
+
+class InputIsNotDirException(Exception):
+    def __init__(self, message = "The provided input tp inputDir is not a str path to a directory that exists") -> None:
+        super().__init__(message)
+
+class InputIsNotFileException(Exception):
+    def __init__(self, message = "The provided input to inputFile is not a str path to a file that exists") -> None:
+        super().__init__(message)
+
+class InputIsNotXlFileException(Exception):
+    def __init__(self, message = "The provided input to inputFile is not a str path to a file that is xls/xlsx") -> None:
+        super().__init__(message)
+
+class OutputIsNotDirException(Exception):
+    def __init__(self, message = "The provided input to outputDir is not a str path to a directory ") -> None:
+        super().__init__(message)
+
+class FastXlsToCsvModuleException(Exception):
+    def __init__(self, message = "There was a script error from within the FastXlsToCsv module, please check for bad input and then report to module developer") -> None:
+        super().__init__(message)
---
+++ build/lib/FastXlsToCsv/__init__.py
@@ -0,0 +1,4 @@
+from FastXlsToCsv import XlsConverter
+from FastXlsToCsv.XlsConverter import convertXlDirToCsv, convertXlFileToCsv
+
+__all__ = ['XlsConverter', 'convertXlDirToCsv', 'convertXlFileToCsv']
